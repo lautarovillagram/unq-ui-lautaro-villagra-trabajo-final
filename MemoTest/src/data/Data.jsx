27c8c82fc50d723f27ce91aa34c9964ced0c6c86
LautaroVillagraM <lautarovillagram@gmail.com>
2024-12-04 20:56:44 -0300

    added data file

---
+++ MemoTest/src/data/Data.jsx
@@ -0,0 +1,23 @@
+  
+const images = import.meta.glob("../assets/cardsImages/*.svg");
+  
+
+const data = {
+    cards: [
+      { id: "0", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "0", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "1", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "1", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "2", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "2", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "3", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "3", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "4", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "4", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "5", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "5", image: images["../assets/cardsImages/alien.svg"] },
+    ],
+  };
+
+
+  export default data;
