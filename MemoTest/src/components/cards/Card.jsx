f5e94d373b94f215826dfc2d1211c5f58d9421d4
LautaroVillagraM <lautarovillagram@gmail.com>
2024-12-04 14:23:43 -0300

    Some cards and grid logic, cards db

--- MemoTest/src/components/cards/Card.jsx
+++ MemoTest/src/components/cards/Card.jsx
@@ -1,22 +1,32 @@
 import logo from '../../assets/react.svg'
-import alien from '../../assets/alien.svg'
 import './Card.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function Card( clicked ) {
-const [isClicked, setIsClicked] = useState(false);
+function Card( {id, image, resetSignal, whenClicked, revealedCards} ) {
+const [isRevealed, setIsRevealed] = useState(false);
+
 
 
 
   const handleClick = () => {
-    setIsClicked(!isClicked); 
+    if (!isRevealed && revealedCards < 2) {
+      setIsRevealed(true); 
+      whenClicked(id);
+    }
   };
+
+  useEffect(() => {
+    if (resetSignal) {
+      setTimeout(() => setIsRevealed(false), 1000);
+      
+    }
+  }, [resetSignal]);
    
-    return(        
-        <div className={`card-wrapper ${isClicked ? 'clicked-card' : 'unclicked-card'}`} onClick={handleClick}  >
-            <img className='card-image' src={isClicked ? logo : alien}/>       
-        </div>
-    );
+  return(        
+    <div className={`card-wrapper ${isRevealed? 'clicked-card' : 'unclicked-card'}`} onClick={handleClick}  >
+      <img className='card-image' src={isRevealed ? image : logo}/>       
+    </div>
+  );
 }
 
 
--- MemoTest/src/components/layout/Grid.jsx
+++ MemoTest/src/components/layout/Grid.jsx
@@ -1,31 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from '../cards/Card.jsx';
 import './Grid.css';
-function Grid() {
-    const [clickedCards, setClickedCards] = useState(0);
+import data from '../../mock/cards.json';
 
+function Grid() {
+    const [clickedCards, setClickedCards] = useState([]);       
+    const [resetSignal, setResetSignal] = useState(false);
+
+    const handleCardClick = (card) => {
+      if (clickedCards.length < 2) { 
+        setClickedCards((prev) => [...prev, card]);
+      }
+    };
+
+    const checkIfSameCard = () => {
+      const [firstCard, secondCard] = clickedCards;
+  
+      if (firstCard.id === secondCard.id) {
+        console.log("same cards");
+      } else {
+        console.log("different cards");
+        triggerReset();
+      }
+      setTimeout(() => setClickedCards([]), 1000);
+      
+    };
+
+    const triggerReset = () => {
+      setResetSignal(true);
+      setTimeout(() => setResetSignal(false), 0); 
+    };
+
+
+    useEffect(() => {
+      if (clickedCards.length === 2) {
+        checkIfSameCard();
+      }
+    }, [clickedCards]);
+   
 
     
     return (
         <>
-        <p>{clickedCards}</p>
         <div className="grid-wrapper">             
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>      
+          {data.cards.map((card, index) => (
+          <Card key={index} id={card.id} image={card.image} revealedCards={clickedCards.length} resetSignal={resetSignal} whenClicked={() =>  handleCardClick({ id: card.id, index })  } /> 
+          ))} 
         </div>
         </>
     );
