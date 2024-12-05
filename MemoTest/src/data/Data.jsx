1ec6bb93c630c1d6956c5d063814bc24d86e5cac
Lautaro Villagra <lautarovillagram@gmail.com>
2024-12-05 19:32:00 -0300

    added some svgs for testing

--- MemoTest/src/data/Data.jsx
+++ MemoTest/src/data/Data.jsx
@@ -1,21 +1,25 @@
-  
-const images = import.meta.glob("../assets/cardsImages/*.svg");
+import alien from '../assets/cardsImages/alien.svg'
+import burger from '../assets/cardsImages/burger.svg'
+import cake from '../assets/cardsImages/cake.svg'
+import chef from '../assets/cardsImages/chef.svg'
+import donut from '../assets/cardsImages/donut.svg'
+import icecream from '../assets/cardsImages/icecream.svg'
   
 
 const data = {
     cards: [
-      { id: "0", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "0", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "1", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "1", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "2", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "2", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "3", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "3", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "4", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "4", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "5", image: images["../assets/cardsImages/alien.svg"] },
-      { id: "5", image: images["../assets/cardsImages/alien.svg"] },
+      { id: "0", image: alien },
+      { id: "0", image: alien },
+      { id: "1", image: burger },
+      { id: "1", image: burger },
+      { id: "2", image: cake },
+      { id: "2", image: cake },
+      { id: "3", image: chef },
+      { id: "3", image: chef },
+      { id: "4", image: donut },
+      { id: "4", image: donut },
+      { id: "5", image: icecream },
+      { id: "5", image: icecream },
     ],
   };
 
