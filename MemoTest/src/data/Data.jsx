ea04608e018543a6d6b7dfa023adc5d5ccd37301
Lautaro Villagra <lautarovillagram@gmail.com>
2024-12-06 13:11:05 -0300

    added cards

--- MemoTest/src/data/Data.jsx
+++ MemoTest/src/data/Data.jsx
@@ -4,6 +4,8 @@ import cake from '../assets/cardsImages/cake.svg'
 import chef from '../assets/cardsImages/chef.svg'
 import donut from '../assets/cardsImages/donut.svg'
 import icecream from '../assets/cardsImages/icecream.svg'
+import container from '../assets/cardsImages/container.svg'
+import pepper from '../assets/cardsImages/pepper.svg'
   
 
 const data = {
@@ -20,6 +22,10 @@ const data = {
       { id: "4", image: donut },
       { id: "5", image: icecream },
       { id: "5", image: icecream },
+      { id: "6", image: container },
+      { id: "6", image: container },
+      { id: "7", image: pepper },
+      { id: "7", image: pepper },
     ],
   };
 
