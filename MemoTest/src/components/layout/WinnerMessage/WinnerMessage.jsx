2a4cc7ec0c80fa0dd49c0f2591420c4cf163213a
LautaroVillagraM <lautarovillagram@gmail.com>
2024-12-07 02:19:28 -0300

    added css import

--- MemoTest/src/components/layout/WinnerMessage/WinnerMessage.jsx
+++ MemoTest/src/components/layout/WinnerMessage/WinnerMessage.jsx
@@ -1,9 +1,13 @@
+import './WinnerMessage.css'
+
 function WinnerMessage({turns, menu, restart}){
     return(
         <>
         <h2>Felicitaciones! completaste el tablero en {turns} turnos</h2>
-        <button onClick={restart}>Reiniciar</button>
-        <button onClick={menu}>menu principal</button>
+        <div className="SP-winner-message-buttons">
+            <button onClick={restart}>Reiniciar</button>
+            <button onClick={menu}>menu principal</button>
+        </div>
         </>
     )
 }
