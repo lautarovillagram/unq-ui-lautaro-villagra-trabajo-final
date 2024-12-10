dcee05d949f8a2d5428d96d0efa3e2645c7275d4
LautaroVillagraM <lautarovillagram@gmail.com>
2024-12-10 15:07:18 -0300

    added all teams

--- MemoTest/src/data/Data.jsx
+++ MemoTest/src/data/Data.jsx
@@ -1,31 +1,106 @@
-import alien from '../assets/cardsImages/alien.svg'
-import burger from '../assets/cardsImages/burger.svg'
-import cake from '../assets/cardsImages/cake.svg'
-import chef from '../assets/cardsImages/chef.svg'
-import donut from '../assets/cardsImages/donut.svg'
-import icecream from '../assets/cardsImages/icecream.svg'
-import container from '../assets/cardsImages/container.svg'
-import pepper from '../assets/cardsImages/pepper.svg'
+import river from '../assets/cardsImages/river.png'
+import botafogo from '../assets/cardsImages/botafogo.png'
+import colocolo from '../assets/cardsImages/colocolo.png'
+import flamengo from '../assets/cardsImages/flamengo.png'
+import fluminense from '../assets/cardsImages/fluminense.png'
+import mineiro from '../assets/cardsImages/mineiro.png'
+import peñarol from '../assets/cardsImages/peñarol.png'
+import talleres from '../assets/cardsImages/talleres.png'
+import bolivar from '../assets/cardsImages/bolivar.png'
+import gremio from '../assets/cardsImages/gremio.png'
+import junior from '../assets/cardsImages/junior.png'
+import nacional from '../assets/cardsImages/nacional.png'
+import sanlorenzo from '../assets/cardsImages/sanlo.png'
+import strongest from '../assets/cardsImages/strongest.png'
+import palmeiras from '../assets/cardsImages/palmeiras.png'
+import estudiantes from '../assets/cardsImages/estudiantes.png'
+import alianzalima from '../assets/cardsImages/alianzalima.png'
+import cobresal from '../assets/cardsImages/cobresal.png'
+import caracas from '../assets/cardsImages/caraca.png'
+import central from '../assets/cardsImages/central.png'
+import huachipato from '../assets/cardsImages/huachipato.png'
+import independiente from '../assets/cardsImages/independiente.png'
+import libertad from '../assets/cardsImages/libertad.png'
+import liverpool from '../assets/cardsImages/liverpool.png'
+import millonarios from '../assets/cardsImages/millonarios.png'
+import palestino from '../assets/cardsImages/palestino.png'
+import tachira from '../assets/cardsImages/tachira.png'
+import barcelona from '../assets/cardsImages/barcelona.png'
+import universitario from '../assets/cardsImages/universitario.png'
+import quito from '../assets/cardsImages/quito.png'
+import saopaulo from '../assets/cardsImages/saopaulo.png'
+import cerroporteño from '../assets/cardsImages/cerroporteño.png'
   
 
 const data = {
     cards: [
-      { id: "0", image: alien },
-      { id: "0", image: alien },
-      { id: "1", image: burger },
-      { id: "1", image: burger },
-      { id: "2", image: cake },
-      { id: "2", image: cake },
-      { id: "3", image: chef },
-      { id: "3", image: chef },
-      { id: "4", image: donut },
-      { id: "4", image: donut },
-      { id: "5", image: icecream },
-      { id: "5", image: icecream },
-      { id: "6", image: container },
-      { id: "6", image: container },
-      { id: "7", image: pepper },
-      { id: "7", image: pepper },
+      { id: "0", image: river },
+      { id: "0", image: river },
+      { id: "1", image: botafogo },
+      { id: "1", image: botafogo },
+      { id: "2", image: colocolo },
+      { id: "2", image: colocolo },
+      { id: "3", image: flamengo },
+      { id: "3", image: flamengo },
+      { id: "4", image: fluminense },
+      { id: "4", image: fluminense },
+      { id: "5", image: talleres },
+      { id: "5", image: talleres },
+      { id: "6", image: peñarol },
+      { id: "6", image: peñarol },
+      { id: "7", image: mineiro },
+      { id: "7", image: mineiro },
+      { id: "8", image: bolivar },
+      { id: "8", image: bolivar },
+      { id: "9", image: cobresal },
+      { id: "9", image: cobresal },
+      { id: "10", image: junior },
+      { id: "10", image: junior },
+      { id: "11", image: nacional },
+      { id: "11", image: nacional },
+      { id: "12", image: palmeiras },
+      { id: "12", image: palmeiras },
+      { id: "13", image: sanlorenzo },
+      { id: "13", image: sanlorenzo },
+      { id: "14", image: gremio },
+      { id: "14", image: gremio },
+      { id: "15", image: strongest },
+      { id: "15", image: strongest },
+      { id: "16", image: estudiantes },
+      { id: "16", image: estudiantes },
+      { id: "17", image: alianzalima },
+      { id: "17", image: alianzalima },
+      { id: "18", image: tachira },
+      { id: "18", image: tachira },
+      { id: "19", image: millonarios },
+      { id: "19", image: millonarios },  
+      { id: "20", image: liverpool },
+      { id: "20", image: liverpool },
+      { id: "21", image: palestino },
+      { id: "21", image: palestino },
+      { id: "22", image: independiente },
+      { id: "22", image: independiente },
+      { id: "23", image: huachipato },
+      { id: "23", image: huachipato },
+      { id: "24", image: caracas },
+      { id: "24", image: caracas },
+      { id: "25", image: libertad },
+      { id: "25", image: libertad },
+      { id: "26", image: central },
+      { id: "26", image: central },
+      { id: "27", image: barcelona },
+      { id: "27", image: barcelona },
+      { id: "28", image: quito },
+      { id: "28", image: quito },
+      { id: "29", image: universitario },
+      { id: "29", image: universitario },
+      { id: "30", image: saopaulo },
+      { id: "30", image: saopaulo },  
+      { id: "31", image: cerroporteño },
+      { id: "31", image: cerroporteño },  
+
+
+      
     ],
   };
 
