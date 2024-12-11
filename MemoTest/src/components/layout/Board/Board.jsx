1c7d4744b86acfa3601dce9f2fb62257f4f8d296
LautaroVillagraM <lautarovillagram@gmail.com>
2024-12-11 01:49:34 -0300

    renamed Board

--- MemoTest/src/components/layout/Board/Board.jsx
+++ MemoTest/src/components/layout/Board/Board.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
 import Grid from "../Grid/Grid.jsx";
-import './SinglePlayerBoard.css'
+import './Board.css'
 import MultiPayerHUD from '../HUDs/MultiPlayerHUD/MultiPlayerHUD.jsx'
 import SinglePlayerHUD from "../HUDs/SinglePlayerHUD/singlePlayerHUD.jsx";
 
-function SinglePlayerBoard({size, toggleMenu, players}) {
+function Board({size, toggleMenu, players}) {
     const [turns, setTurns] = useState(0);
     const [revealedPairs, setRevealedPairs] = useState(0);
     var pairs = (size * size) / 2;
@@ -53,4 +53,4 @@ function SinglePlayerBoard({size, toggleMenu, players}) {
     )
 }
 
-export default SinglePlayerBoard;
+export default Board;
